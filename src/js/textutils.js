940221ffc361fc84700d5345d4439ed0a7458737
Mihai Bazon <mihai@bazon.net>
2009-02-08 12:08:13 +0200

    text filling supports paragraphs delimited by ">+" or "|+"

--- src/js/textutils.js
+++ src/js/textutils.js
@@ -4,19 +4,19 @@ DlTextUtils = (function(){
 
         var D, DOM = DynarchDomUtils, START_REGEXPS = [
 
-                /^(\s*[-*]+\s+)/, function(m) {
+                (/^(\s*[-*]+\s+)/), function(m) {
                         return [ m[0], " ".x(m[0].length), m[0].length ];
                 },
 
-                /^(\s*[0-9]+\.\s+)/, function(m) {
+                (/^(\s*[0-9]+\.\s+)/), function(m) {
                         return [ m[0], " ".x(m[0].length), m[0].length ];
                 },
 
-                /^\s*([>|]\s*)*/, function(m) {
+                (/^\s*([>|]\s*)*/), function(m) {
                         return [ m[0], m[0], m[0].length, /\n\s*([>|]\s*)*/g, "\n" ];
                 },
 
-                /^\s+/, function(m) {
+                (/^\s+/), function(m) {
                         return [ m[0], m[0], m[0].length ];
                 }
 
@@ -28,22 +28,48 @@ DlTextUtils = (function(){
 
                 // M-Q (fill-paragraph)
                 if (ev.altKey && ev.charCode == 113) {
-                        var val = D.fillText(this.value, 72, DOM.getSelectionRange(this).start);
+                        var val = D.fillText(this.value, 72, DOM.getSelectionRange(this).start),
+                                scroll = { x: this.scrollLeft, y: this.scrollTop };
                         this.value = val.text;
                         DOM.setSelectionRange(this, val.pos, val.pos);
+                        this.scrollLeft = scroll.x;
+                        this.scrollTop = scroll.y;
                         return false;
                 }
+        };
+
+        var RE_PARA = /\n([>|\s]*\n)+/g;
+        function lastIndexOfRegexp(str, re, caret) {
+                var m, pos = -1;
+                re.lastIndex = 0;
+                re.global = true;
+                var last_post = -1;
+                while (m = re.exec(str)) {
+                        if (re.lastIndex >= caret)
+                                break;
+                        pos = re.lastIndex;
+                        if (pos == last_post) {
+                                throw "Repeated! " + pos;
+                        }
+                        last_post = pos;
+                }
+                return pos;
+        };
+
+        function nextIndexOfRegexp(str, re, caret) {
+                re.lastIndex = caret;
+                re.global = true;
+                var m = re.exec(str);
+                return m ? m.index : null;
         };
 
         return D = {
 
                 getParagraph: function(text, pos) {
-                        var start = text.lastIndexOf("\n\n", pos - 1), end = text.indexOf("\n\n", pos);
+                        var start = lastIndexOfRegexp(text, RE_PARA, pos + 1), end = nextIndexOfRegexp(text, RE_PARA, pos);
                         if (start == -1)
                                 start = 0;
-                        else
-                                start += 2;
-                        if (end == -1)
+                        if (end == null)
                                 end = text.length;
                         return { start: start, end: end, text: text.substring(start, end) };
                 },
@@ -63,8 +89,15 @@ DlTextUtils = (function(){
                         var a = D.getFillPrefix(para), prefix = a[1], restPos = a[2];
                         var before = para.substr(0, restPos);
                         para = para.substr(restPos);
-                        if (a[3])
-                                para = para.replace(a[3], a[4] || "");
+                        var d = 0;
+                        if (a[3]) {
+                                para = para.replace(a[3], function(s) {
+                                        var r = a[4] || "";
+                                        d = s.length - r.length;
+                                        return r;
+                                });
+                        }
+                        d = prefix.length - d;
                         para = para.replace(/\n/g, " ").replace(/([^.])\s\s+/g, "$1 ");
                         var re = new RegExp("(.{0," + (width - prefix.length) + "})(\\s+|$)", "g");
                         var m, buf = [], lastPos = 0, line, posDiff = 0;
@@ -75,13 +108,12 @@ DlTextUtils = (function(){
                                         line = m[1];
                                 lastPos = re.lastIndex;
                                 if (lastPos < pos)
-                                        posDiff++;
+                                        posDiff += d;
                                 if (!/\S/.test(line))
                                         break;
                                 buf.push(line.trim(true));
                         }
-                        // XXX: should compute the point (pos) difference
-                        return { text: before + buf.join("\n" + prefix), posDiff: posDiff * prefix.length };
+                        return { text: before + buf.join("\n" + prefix), posDiff: posDiff };
                 },
 
                 fillText: function(text, width, pos) {
@@ -89,7 +121,7 @@ DlTextUtils = (function(){
                         var before = text.substr(0, p.start), after = text.substr(p.end);
                         var posInPara = pos - p.start;
                         var ret = D.fillParagraph(p.text, width, posInPara);
-                        return { text: before + ret.text + after, pos: pos };
+                        return { text: before + ret.text + after, pos: pos + ret.posDiff };
                 },
 
                 emacsipateTextarea: function(ta) {
