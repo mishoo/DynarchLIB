05699c3b80ab0d32cc13984856cff00f1c7218e7
Mihai Bazon <mihai@bazon.net>
2011-04-14 11:51:40 +0300

    trim data in email validator

--- src/js/validator.js
+++ src/js/validator.js
@@ -96,9 +96,9 @@ DEFINE_CLASS("DlValidator", null, function(D, P){
 
         // very dumb Email validator
         D.Email = function(data) {
+                data = data.trim();
                 if (!/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i.test(data)) {
-                        throw new DlValidatorException("That doesn't look like an email address",
-                                                       DlValidatorException.MISMATCH);
+                        throw new DlValidatorException("That doesn't look like an email address", DlValidatorException.MISMATCH);
                 }
                 return data;
         };
